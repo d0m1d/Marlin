42636aa8cd92a4e21af2de952c22255898dfb4b5
Dominic <domi.d@gmx.at>
2022-05-22 09:20:20 +0200

    minor: xy size, home

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1375,7 +1375,7 @@
 //#define Z_HOMING_HEIGHT  4      // (mm) Minimal Z height before homing (G28) for Z clearance above the bed, clamps, ...
                                   // Be sure to have this much clearance over your Z_MAX_POS to prevent grinding.
 
-//#define Z_AFTER_HOMING  10      // (mm) Height to move to after homing Z
+#define Z_AFTER_HOMING  Z_MAX_POS - 10      // (mm) Height to move to after homing Z
 
 // Direction of endstops when homing; 1=MAX, -1=MIN
 // :[-1,1]
@@ -1389,8 +1389,8 @@
 // @section machine
 
 // The size of the printable area
-#define X_BED_SIZE 440
-#define Y_BED_SIZE 440
+#define X_BED_SIZE 450
+#define Y_BED_SIZE 450
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
